f2a5079fe32071885e5b8b0fa37094182ec48e97
vhiveed <ea09165418562@gmail.comgit config --global user.email ea09165418562@gmail.com>
2026-06-29 22:19:49 +0100

    fix filename

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/crimeDB")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
